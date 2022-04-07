eddc2c0ba38ac039af35a5fafe2883d3df27de19
pcfds <castrofeijoopedro@gmail.com>
2022-04-07 15:14:49 -0300

    updating API

--- APIRestOdontologo/src/main/java/clinica/dh/controller/OdontologoController.java
+++ APIRestOdontologo/src/main/java/clinica/dh/controller/OdontologoController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/odontologos")
 public class OdontologoController {
@@ -16,14 +18,33 @@ public class OdontologoController {
     private OdontologoService odontologoService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<OdontologoDTO> findById(@PathVariable("id") Integer id){
-        OdontologoDTO odontologoDTO=odontologoService.findById(id);
+    public ResponseEntity<OdontologoDTO> findById(@PathVariable("id") Integer id) {
+        OdontologoDTO odontologoDTO = odontologoService.findById(id);
         return new ResponseEntity<>(odontologoDTO, HttpStatus.OK);
     }
 
     @PostMapping()
-    public ResponseEntity<OdontologoDTO> create(@RequestBody OdontologoDTO odontologoDTO){
-        OdontologoDTO newOdontologoDTO=odontologoService.create(odontologoDTO);
+    public ResponseEntity<OdontologoDTO> create(@RequestBody OdontologoDTO odontologoDTO) {
+        OdontologoDTO newOdontologoDTO = odontologoService.create(odontologoDTO);
         return new ResponseEntity<>(newOdontologoDTO, HttpStatus.OK);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteByid(@PathVariable("id") Integer id) {
+        odontologoService.deleteById(id);
+        return new ResponseEntity<>("Odontologo eliminado", HttpStatus.OK);
+    }
+
+    @PutMapping()
+    public ResponseEntity<OdontologoDTO> update(@RequestBody OdontologoDTO odontologoDTO) {
+        OdontologoDTO newOdontologoDTO = odontologoService.update(odontologoDTO);
+        return new ResponseEntity<>(newOdontologoDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/List")
+    public ResponseEntity<List<OdontologoDTO>> findAll() {
+        List<OdontologoDTO> odontologoDTOList = odontologoService.findAll();
+        return new ResponseEntity<>(odontologoDTOList, HttpStatus.OK);
+    }   
 }
+
