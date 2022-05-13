1d7a8f50bda6c8956ce143d0a05b2ca2a6ead32d
pcfds <castrofeijoopedro@gmail.com>
2022-05-13 12:33:31 -0300

    trying out JWT on my project

---
+++ FinalProyectAPIDentalClinic/src/main/java/ApiRestFinalProject/DentalClinic/Login/JwtUtil.java
@@ -0,0 +1,58 @@
+package ApiRestFinalProject.DentalClinic.Login;
+
+import io.jsonwebtoken.*;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+
+public class JwtUtil {
+    private String SECRET_KEY = "secret";
+
+    public String extractUserName(String token) {
+        return extractClaimUsername(token);
+    }
+
+    public java.util.Date extractExpiration(String token) {
+        return extractClaimDate(token);
+    }
+
+    public java.util.Date extractClaimDate(String token){
+        Claims claims = extractAllClaims(token);
+        return claims.getExpiration();
+    }
+
+    public String extractClaimUsername(String token){
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new java.util.Date(System.currentTimeMillis()))
+                .setExpiration(new java.util.Date(System.currentTimeMillis() + 1000 * 60 +60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+}
