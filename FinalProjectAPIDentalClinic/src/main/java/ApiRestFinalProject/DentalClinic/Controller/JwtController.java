34ed8bb48c581eb557710296aaa2edf7f4899149
pcfds <castrofeijoopedro@gmail.com>
2022-05-13 17:00:40 -0300

    working with jwt

---
+++ FinalProjectAPIDentalClinic/src/main/java/ApiRestFinalProject/DentalClinic/Controller/JwtController.java
@@ -0,0 +1,48 @@
+package ApiRestFinalProject.DentalClinic.Controller;
+
+import ApiRestFinalProject.DentalClinic.Login.AppUserService;
+import ApiRestFinalProject.DentalClinic.Login.Authentication.AuthenticationRequest;
+import ApiRestFinalProject.DentalClinic.Login.Authentication.AuthenticationResponse;
+import ApiRestFinalProject.DentalClinic.Login.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class JwtController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private AppUserService userDetailsService;
+    @Autowired
+    private JwtUtil JwtUtil;
+
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        }catch (BadCredentialsException e) {
+            throw new Exception("Incorrect", e);
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = JwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse((jwt)));
+    }
+
+    @RequestMapping({"/hello"})
+    public String hello() {
+        return "Hello World";
+    }
+}
---
+++ FinalProjectAPIDentalClinic/src/main/java/ApiRestFinalProject/DentalClinic/Login/Authentication/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package ApiRestFinalProject.DentalClinic.Login.Authentication;
+
+public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
